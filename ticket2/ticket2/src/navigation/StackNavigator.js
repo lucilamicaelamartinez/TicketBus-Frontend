73105bcdfef7cca1d1e8a0dddc1b1c5039a2a3a3
lucilamicaelamartinez <lucilamicaelamartinez@gmail.com>
2023-10-24 21:22:15 -0300

    last reservations screens working

--- ticket2/ticket2/src/navigation/StackNavigator.js
+++ ticket2/ticket2/src/navigation/StackNavigator.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
 import { HomeScreen } from '../screens/HomeScreen'
 import { ProfileScreen } from '../screens/ProfileScreen'
+import { LastReservations } from '../screens/LastReservationsScreen'
 
 export const StackNavigator = () => {
 
@@ -17,6 +18,7 @@ export const StackNavigator = () => {
 
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="Profile" component={ProfileScreen} />
+      <Stack.Screen name="LastReservations" component={LastReservations} />
 
     </Stack.Navigator>
 
